6bc570373c246b8742af65e43962b48258c681bc
320119020 <anilkumar.b@philips.com>
2022-02-07 19:07:17 +0530

    first commit

---
+++ src/com/arr/DuplicatesRemoveInAnArray.java
@@ -0,0 +1,27 @@
+package com.arr;
+
+import java.util.Arrays;
+
+public class DuplicatesRemoveInAnArray {
+	public static void main(String[] args) {
+		int arr[] = { 1, 2, 3, 4, 5, 6, 3, 4 };
+		int n = removeDuplicates(arr);
+		System.out.println("The Result Array is:");
+		for (int i = 0; i < n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+	private static int removeDuplicates(int arr[]) {
+		Arrays.sort(arr);
+		int j = 0;
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i] != arr[i + 1]) {
+				arr[j++] = arr[i];
+			}
+		}
+		arr[j++] = arr[arr.length - 1];
+
+		return j;
+	}
+}
